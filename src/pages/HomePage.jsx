8581cefeacfd0e33b8e623b6390cd19a550eb642
xh3t4g <xh3t4g@gmail.com>
2026-04-11 18:59:30 +0300

    Поменял главную страницу, теперь можно просто дополнить json файл и будет готовый проект

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -23,34 +23,37 @@ export function HomePage () {
     <main>
         <section className="section_Our_services">
             <div className="card_container">
-                <div className="card_trova">
-                    <div className="card_container_title_trova">
-                    <h3>🌿 Покос травы</h3></div>
+                <div className="card_Services">
+                    <div className="card_container_title_services">
+                    <h3>Чем можем помочь</h3></div>
 
-                    <ul className='card_services_list trova_list'>
+                    <ul className='card_services_list services_list'>
                         <li>
-                            <Link to="/service/kosim_travy" onClick={scrollToTop}>Косим траву</Link>
+                            <Link to="/service/pokos_travi" onClick={scrollToTop}>🌿 Покос травы</Link>
                         </li>
-                        <li>Убираем траву</li>
-                        <li>Вывозим мусор</li>
-                        <li>Чистим участок</li>
-                        <li>Подравниваение</li>
-                        <li>Удаление сорняков</li>
+                        <li>🗑️ Вывоз мусора</li>
+                        <li>🧹 Чистка участка</li>
+                        <li>🪓 Вырубка деревьев</li>
+                        <li>🔨 Демонтаж</li>
+                        <li>🔧 Разнорабочие</li>
                     </ul>
                 </div>
 
-                <div className="card_bild">
-                    <div className="card_container_title_bild">
-                        <h3>🛠 Разнорабочие</h3>
+                <div className="Card_contact_us">
+                    <div className="card_container_title_contacts">
+                        <h3>Обсудить заказ</h3>
                     </div>
 
-                    <ul className="card_services_list bild_list">
-                        <li>Вырубка деревьев</li>
-                        <li>Демонтаж</li>
-                        <li>Вывоз хлама</li>
-                        <li>Уборка после работ</li>
-                        <li>Перенос и погрузка</li>
-                        <li>Мелкий ремонт</li>
+                    <ul className="card_services_list_b">
+                        <li>
+                            <a href="https://t.me/cnskfho" className="Telegram" target="_blank" rel="noopener">Написать в Telegram</a>
+                        </li>
+                        <li>
+                            <a href="https://wa.me/79931849236" className="WhatsApp" target="_blank" rel="noopener">Написать в WhatsApp</a>
+                        </li>
+                        <li>
+                            <a href="tel:+79931849236" className="Phone" target="_blank" rel="noopener">Позвонить</a>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -62,14 +65,6 @@ export function HomePage () {
                         behavior: 'smooth'
                     }), 50)}>Наши работы</Link>
                 </nav>
-            </div>
-
-            <div className="news">
-                <a href="https://t.me/cnskfho" className="Telegram" target="_blank" rel="noopener">Написать в Telegram</a>
-
-                <a href="https://wa.me/79931849236" className="WhatsApp" target="_blank" rel="noopener">Написать в WhatsApp</a>
-
-                <a href="tel:+79931849236" className="Phone" target="_blank" rel="noopener">Позвонить</a>
             </div>
         </section>
     </main>
